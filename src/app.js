3d48e8f448f3278ce39c235bed23b943d925f117
aka5h-u <akashu97@gmail.com>
2025-05-01 11:25:40 +0530

    Added validaotrs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 require("./config/database");
 const User = require("./models/user");
+
 const app = express();
 
 app.use(express.json());
@@ -14,7 +15,61 @@ app.post("/signup", async (req, res) => {
   } catch (err) {
     res.status(400).send("Error saving the user" + err.message);
   }
-  console.log(req.body);
+  // console.log(req.body);
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body._id;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+
+    res.send("User deleted");
+  } catch (error) {
+    res.status(400).send("User not found");
+  }
+});
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "gender"];
+
+    const isUpdateAllowed = Object.keys(req.body).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Updates not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Cannot add more skills");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, req.body, {
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Update failed " + error.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,65 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+var validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://picsum.photos/id/237/200/300",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is the default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
